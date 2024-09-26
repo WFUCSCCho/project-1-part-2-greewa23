f68afdaf4bf8db0106fe4fa5c212224a5d4e1544
greewa23 <greewa23@wfu.edu>
2024-09-26 10:41:02 -0400

    I created my Volcano class with its fields and methods. Then I edited the Parser class to create a bst of my custom class objects. I edited Proj1 to reflect the two files needed for input.

--- src/Proj1.java
+++ src/Proj1.java
@@ -1,11 +1,22 @@
+/**********************************************************************
+ * @file proj1.java
+ * @brief This program implements the Proj1 class. The class reads
+ * in the two input files from the command line. Kaggle Dataset:
+ * https://www.kaggle.com/datasets/ramjasmaurya/volcanoes-on-earth-in-2021
+ * @author Wynne Greene
+ * @date: September 26, 2024
+ ***********************************************************************/
 import java.io.FileNotFoundException;
 
 public class Proj1 {
     public static void main(String[] args) throws FileNotFoundException{
-        if(args.length != 1){
+        //Check if there are two command line arguments.
+        if(args.length != 2){
             System.err.println("Argument count is invalid: " + args.length);
             System.exit(0);
         }
-        new Parser(args[0]);
+        //Call Parser with the input.txt and csv file of data.
+        new Parser(args[0], args[1]);
+
     }
 }
